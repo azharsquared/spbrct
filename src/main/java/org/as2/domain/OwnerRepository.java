b501092d9b4edb80f7dacfee4f4fb3743237a6c0
azharsquared <azharsquared@gmail.com>
2024-08-23 11:44:49 +0530

    test: owner

--- src/main/java/org/as2/domain/OwnerRepository.java
+++ src/main/java/org/as2/domain/OwnerRepository.java
@@ -3,8 +3,12 @@ package org.as2.domain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 // @RepositoryRestResource is used to customize the REST endpoint
 @RepositoryRestResource(path="owners-rest")
 public interface OwnerRepository extends
         CrudRepository<Owner, Long> {
+
+    Optional<Owner> findByFirstname(String firstName);
 }
--- src/test/java/org/as2/MainTest.java
+++ src/test/java/org/as2/MainTest.java
@@ -1,6 +1,7 @@
 package org.as2;
 
 import org.as2.web.VehicleController;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +11,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class MainTest {
 
+    //https://docs.spring.io/spring-framework/reference/testing/testcontext-framework/fixture-di.html.
     @Autowired
     private VehicleController controller;
     @Test
+    @DisplayName("First example test case")
     void contextLoads() {
         assertThat(controller).isNotNull();
     }
