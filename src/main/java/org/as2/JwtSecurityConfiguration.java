075a2a445ab96d0931217b6bf2a203ed2e2a5b3f
azharsquared <azharsquared@gmail.com>
2024-10-01 23:25:57 +0530

    feat: security config

--- src/main/java/org/as2/JwtSecurityConfiguration.java
+++ src/main/java/org/as2/JwtSecurityConfiguration.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -51,8 +49,18 @@ public class JwtSecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws
             Exception {
         http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorizeHttpRequests) ->
-                        authorizeHttpRequests.anyRequest().permitAll());
+                        authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated())
+                .addFilterBefore(authenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling((exceptionHandling) ->
+                        exceptionHandling.authenticationEntryPoint(exceptionHandler));
+//        http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
+//                .authorizeHttpRequests((authorizeHttpRequests) ->
+//                        authorizeHttpRequests.anyRequest().permitAll());
 //        http.csrf(AbstractHttpConfigurer::disable) //(csrf) -> csrf.disable()
 //                .cors(withDefaults())
 //                .sessionManagement((sessionManagement) -> sessionManagement.
